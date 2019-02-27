4750eded5060ff9c585f7c57edfd14cd5ef37683
ryanw <ryanw@128.4.63.72>
2019-02-26 22:52:39 -0500

    minor change. Watch pt. 2 video.

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -6,15 +6,15 @@ public class LatinSquare {
 	private int [][] LatinSquare;
 
 	public LatinSquare(int[][] puzzle) {
-		super(); //what does super mean?
+		super(); //what does super mean? no need for this. since the variables are different.
 		LatinSquare = puzzle;
 	}
 
-	public int[][] getLatinSquare() {
+	public int[][] getLatinSquare() { //The getter method that will call this square.
 		return LatinSquare;
 	}
 
-	public void setLatinSquare(int[][] latinSquare) {
+	public void setLatinSquare(int[][] latinSquare) { // The setter method.
 		LatinSquare = latinSquare;
 	}
 	public static boolean doesElementExist(int[] arr, int iValue) { //First three words, Access modifier, ???, return type.
@@ -27,6 +27,9 @@ public class LatinSquare {
 		return false;
 }
 	public static boolean hasDuplicates (int[] arr) {
+		if (arr == null) {
+			return false;
+		}
 		int [] arrCopy = Arrays.copyOf(arr, arr.length); //this saves the original array from being changed completly.
 		Arrays.sort(arr); // Arrays are passed by reference. Once the array is passed through this method, it will remain sorted and not revert back to its original form.
 		
